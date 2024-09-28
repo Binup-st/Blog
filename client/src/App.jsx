250d1abbba3f4ad323eb20e2c3f21f36db5425a9
Binup-st <2077csit07@hcoe.edu.np>
2024-09-28 15:56:45 +0545

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
-    <div className='text-2xl text-red-500'>
-      Mern Blog
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/projects' element={<Projects/>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
