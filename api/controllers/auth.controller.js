b2baf092683103b274d52b78b62e9475e4eaff3d
Binup-st <2077csit07@hcoe.edu.np>
2024-10-04 00:16:28 +0545

    add middleware and error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res)=>{
+export const signup = async (req, res, next)=>{
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username==='' || password === '' || email === ''){
-        return res.status(400).json({message: "All field must be filled"})
+        next(errorHandler(400, 'All fields are required'))
     } 
 
     const hashPassword = bcryptjs.hashSync(password, 10);
@@ -19,7 +20,7 @@ export const signup = async (req, res)=>{
         res.json({message:"Signup Successful"})
     }
     catch(err){
-        res.status(500).json({message: err.message})
+       next(err)
     }
 
 }
